73938f7d62891d325702f301c141ca77c66dea05
dnlml <info@danielemeli.com>
2017-02-23 17:41:26 +0100

    better meta for RWD

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="stylesheet" media="screen" href="assets/styles/main.css">
   <title>Title</title>
   <script>
